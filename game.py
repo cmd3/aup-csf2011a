200b70958cf0e1e3eb55f196a4618debbe7e8fb4
christian desderio <cmd3@csserver2.(none)>
2011-08-08 14:11:53 +0800

    midterm

--- game.py
+++ game.py
@@ -1,20 +1,3 @@
 x = random(3)
-loop = 0
-print"Let's play game"
-print"Can you guess my number(1-1000)?"
-y = int(raw_input(How many trials did you want?  ))
 
-guess = int(raw_input(Enter the number : ))
-
-while (loop != y):
-
-	if (guess < x):
-		print"Lower!"
-	elif (guess > x):
-		print"Higher!"
-	else:
-		print"You Got it!"
-
-print"Game Over!"
-		
-
+print x
